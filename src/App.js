e142013141d6ffc790f8c47df4ef3a80576bf322
gaston <gaston_a96@hotmail.com>
2022-07-20 02:05:16 -0300

    fixing env vars

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import SearchIcon from './resources/search.svg';
 
 import MovieCard from './components/MovieCard'; 
 
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
-const API_URL = `https://imdb-api.com/API/Search/${process.env.API_KEY}/`;
-const API_URL_TOP = `https://imdb-api.com/en/API/MostPopularMovies/${process.env.API_KEY}/`;
+const API_URL = `https://imdb-api.com/API/Search/${API_KEY}/`;
+const API_URL_TOP = `https://imdb-api.com/en/API/MostPopularMovies/${API_KEY}/`;
 
 const App = () => {
 
