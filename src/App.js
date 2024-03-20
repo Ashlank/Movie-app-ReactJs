f9ce4d405fe5229f030b114e7d8efada3dcbc5d8
Gastón Alvarez <gaston_a96@hotmail.com>
2024-03-19 23:39:45 -0300

    fix api

--- src/App.js
+++ src/App.js
@@ -1,79 +1,78 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
 
-import './css/app.css';
-import SearchIcon from './resources/search.svg';
+import "./css/app.css";
+import SearchIcon from "./resources/search.svg";
 
-import MovieCard from './components/MovieCard'; 
+import MovieCard from "./components/MovieCard";
 
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_TOKEN = process.env.API_TOKEN
 
-const API_URL = `https://imdb-api.com/API/Search/${API_KEY}/`;
-const API_URL_TOP = `https://imdb-api.com/en/API/MostPopularMovies/${API_KEY}/`;
+const options = {
+	method: "GET",
+	headers: {
+		accept: "application/json",
+		Authorization: API_TOKEN,
+	},
+};
+
+const API_URL = "https://api.themoviedb.org/3/movie/popular";
+const SEARCH_URL = "https://api.themoviedb.org/3/search/movie";
 
 const App = () => {
+	const [movies, setMovies] = useState([]);
+	const [searchTerm, setSearchTerm] = useState("");
 
-    const [movies, setMovies] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
+	const searchMovies = async (title) => {
+		if (title) {
+			const response = await fetch(`${SEARCH_URL}?query=${title}`, options);
+			const { results } = await response.json();
+			setMovies(results);
+            console.log(results)
+		} else {
+			const response = await fetch(`${API_URL}`, options);
+			const { results } = await response.json();
+			setMovies(results);
+		}
+	};
 
-    const searchMovies = async (title) => {
-        if(title){
-            const response = await fetch(`${API_URL}${title}`);
-            const data = await response.json();
-            setMovies(data.results);
-            console.log(data.results);
-        } else{
-            const response = await fetch(`${API_URL_TOP}`);
-            const data = await response.json();
-            data.items.map( (item) => {
-                return item.image = item.image.split('V1', 1) + '.jpg';
-            })
-
-            setMovies(data.items);
-            console.log(data.items);
-        }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        searchMovies(searchTerm)
     }
 
+	useEffect(() => {
+		searchMovies();
+	}, []);
 
-    useEffect(() => {
-        searchMovies();
-    }, []);
-
-
-    return(
-        <>  
-            <div className='container app'>
-                <h1>Movie App</h1>
-                <div className='search container'>
-                    <input 
-                        placeholder='Buscar película' 
-                        value={searchTerm} 
-                        onChange={(e) => setSearchTerm(e.target.value)} 
-                    />
-                    <img 
-                        src={SearchIcon}
-                        alt='search icon'
-                        onClick={() => searchMovies(searchTerm)}
-                    />
-                </div>
-                <div className='container movies'>
-                    {   
-                        movies?.length > 0                             
-                            ? (    
-                                movies.map( (movie) => (
-                                    <MovieCard movie={ movie } key={movie.id}/>
-                                ))
-                            ) : (
-                                <div>
-                                    <p>No se encontraron peliculas!</p>
-                                </div>
-                            )
-                    }
-                </div>
-
-            </div>
-        </>
-    );
+	return (
+		<>
+			<div className="container app">
+				<h1>Movie App</h1>
+				<div className="search container">
+					<form onSubmit={handleSubmit}>
+						<input
+							placeholder="Buscar película"
+							value={searchTerm}
+							onChange={(e) => setSearchTerm(e.target.value)}
+						/>
+						<button type="button">
+							<img src={SearchIcon} alt="search icon" />
+						</button>
+					</form>
+				</div>
+				<div className="container movies">
+					{movies?.length > 0 ? (
+						movies.map((movie) => <MovieCard movie={movie} key={movie.id} />)
+					) : (
+						<div>
+							<p>No se encontraron peliculas!</p>
+						</div>
+					)}
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default App;
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 
-const MovieCard = ({ movie: { id, title, year, description, image } }) => {
+const MovieCard = ({ movie: { id, title, release_date, overview, poster_path } }) => {
+    const imageUrl = 'https://image.tmdb.org/t/p/w342/'
 
-    return (
-        <div className="movie" id={id}>
-            <div> 
-                <p>{year ? year : description}</p>
-            </div>
-            <div>
-                <img src={image !== null ? image : 'https://via.placeholder.com/400'} alt="Movie Poster"></img>
-            </div>
-            <div>
-                <h3>{title}</h3>
-            </div>
-
-        </div>
-    );
-}
+	return (
+		<div className="movie" id={id}>
+			<div>
+				<p>{release_date ? release_date : overview}</p>
+			</div>
+			<div>
+				<img
+					src={poster_path ? `${imageUrl}${poster_path}` : "https://placehold.co/400"}
+					alt="Movie Poster"
+				></img>
+			</div>
+			<div>
+				<h3>{title}</h3>
+			</div>
+		</div>
+	);
+};
 
 export default MovieCard;
